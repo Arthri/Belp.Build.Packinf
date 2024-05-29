52cf886c81111421db1b3ffd5ecf49f78bd1061d
Arthri <41360489+a@users.noreply.github.com>
2024-05-29 03:23:24 +0000

    Add tests for push dev package

---
+++ tests/Belp.Build.Packinf.UnitTests/DevelopmentPackageTests.cs
@@ -0,0 +1,90 @@
+﻿using Belp.Build.Testing;
+using Belp.Build.Testing.Resources;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Build.Execution;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace Belp.Build.Packinf.UnitTests;
+
+public class DevelopmentPackageTests
+{
+    [Fact]
+    public void Expect_package_to_be_pushed_and_cache_to_be_cleared()
+    {
+        TestSampleInstance sample = MSBuildTest.Load.Sample("PushDevelopmentPackage");
+        Directory.CreateDirectory(Path.Combine(sample.RootPath, "bin", "packages"));
+
+        {
+            FileTestProject.Instance project = sample.DefaultProject;
+            MSBuildResult result = project.Build(
+                configureRequestData: request => request.TargetNames.Add("Pack"),
+                configureProjectInstance: project => project.SetProperty("PushPackageToDevelopmentSource", "true")
+            );
+
+            using var scope = new AssertionScope();
+            result.OverallResult.Should().Be(BuildResultCode.Success);
+            result.Diagnostics.Should().BeEmpty();
+
+            AssertPackagesEqual();
+        }
+
+        {
+            FileTestProject.Instance project = sample.Projects.First(p => p.TestProject.Name == "PackageUsage.csproj");
+            MSBuildResult result = project.Build(BuildRequestDataFlags.ProvideProjectStateAfterBuild);
+
+            using var scope = new AssertionScope();
+            result.OverallResult.Should().Be(BuildResultCode.Success);
+            result.Diagnostics.Should().BeEmpty();
+
+            string packageCache = result.Properties
+                .Should().ContainKey("NuGetPackageRoot")
+                .WhoseValue.Should().NotBeNullOrEmpty()
+                .And.Subject
+                ;
+            Directory.Exists(Path.Combine(packageCache, "PushDevelopmentPackage", "1.0.0")).Should().BeTrue("Package should be restored and cached in global package cache");
+        }
+
+        {
+            FileTestProject.Instance project = sample.DefaultProject;
+            MSBuildResult result = project.Build(
+                BuildRequestDataFlags.ProvideProjectStateAfterBuild,
+                configureRequestData: request => request.TargetNames.Add("Pack"),
+                configureProjectInstance: project => project.SetProperty("PushPackageToDevelopmentSource", "true")
+            );
+
+            using var scope = new AssertionScope();
+            result.OverallResult.Should().Be(BuildResultCode.Success);
+            result.Diagnostics.Should().BeEmpty();
+
+            string packageCache = result.Properties
+                .Should().ContainKey("NuGetPackageRoot")
+                .WhoseValue.Should().NotBeNullOrEmpty()
+                .And.Subject
+                ;
+            Directory.Exists(Path.Combine(packageCache, "PushDevelopmentPackage", "1.0.0")).Should().BeFalse("Package should be wiped from global package cache");
+
+            AssertPackagesEqual();
+        }
+
+        void AssertPackagesEqual()
+        {
+            Span<byte> hashOriginal = stackalloc byte[64];
+            Span<byte> hashNew = stackalloc byte[64];
+
+            GetHash("Debug/PushDevelopmentPackage.1.0.0.nupkg", hashOriginal);
+            GetHash("packages/PushDevelopmentPackage.1.0.0.nupkg", hashNew);
+
+            void GetHash(string filePath, scoped Span<byte> output)
+            {
+                string path = Path.Combine(sample.RootPath, "bin", filePath);
+                File.Exists(path).Should().BeTrue($"File {path} should exist");
+                using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                SHA512.HashData(stream, output);
+            }
+
+            hashOriginal.SequenceEqual(hashNew).Should().BeTrue("NuGet package in bin should be equal to pushed NuGet package");
+        }
+    }
+}
