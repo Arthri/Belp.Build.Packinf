65949ceb171398e4fbdb64afe078141782d0ec7d
Arthri <41360489+a@users.noreply.github.com>
2024-06-07 18:15:02 +0000

    Fix tests

--- tests/Belp.Build.Packinf.UnitTests/DevelopmentPackageTests.cs
+++ tests/Belp.Build.Packinf.UnitTests/DevelopmentPackageTests.cs
@@ -13,7 +13,7 @@ public class DevelopmentPackageTests
     [Fact]
     public void Expect_package_to_be_pushed_and_cache_to_be_cleared()
     {
-        TestSampleInstance sample = MSBuildTest.Load.Sample("PushDevelopmentPackage");
+        TestSampleInstance sample = MSBuildTest.Load.Sample("PushPackage");
         Directory.CreateDirectory(Path.Combine(sample.Directory, "bin", "packages"));
 
         {
@@ -42,7 +42,7 @@ public class DevelopmentPackageTests
                 .WhoseValue.Should().NotBeNullOrEmpty()
                 .And.Subject
                 ;
-            Directory.Exists(Path.Combine(packageCache, "PushDevelopmentPackage", "1.0.0")).Should().BeTrue("Package should be restored and cached in global package cache");
+            Directory.Exists(Path.Combine(packageCache, "PushPackage", "1.0.0")).Should().BeTrue("Package should be restored and cached in global package cache");
         }
 
         {
@@ -61,7 +61,7 @@ public class DevelopmentPackageTests
                 .WhoseValue.Should().NotBeNullOrEmpty()
                 .And.Subject
                 ;
-            Directory.Exists(Path.Combine(packageCache, "PushDevelopmentPackage", "1.0.0")).Should().BeFalse("Package should be wiped from global package cache");
+            Directory.Exists(Path.Combine(packageCache, "PushPackage", "1.0.0")).Should().BeFalse("Package should be wiped from global package cache");
 
             AssertPackagesEqual();
         }
@@ -71,12 +71,12 @@ public class DevelopmentPackageTests
             Span<byte> hashOriginal = stackalloc byte[64];
             Span<byte> hashNew = stackalloc byte[64];
 
-            GetHash("Debug/PushDevelopmentPackage.1.0.0.nupkg", hashOriginal);
-            GetHash("packages/PushDevelopmentPackage.1.0.0.nupkg", hashNew);
+            GetHash("PushPackage/bin/Debug/PushPackage.1.0.0.nupkg", hashOriginal);
+            GetHash("bin/packages/PushPackage.1.0.0.nupkg", hashNew);
 
             void GetHash(string filePath, scoped Span<byte> output)
             {
-                string path = Path.Combine(sample.Directory, "bin", filePath);
+                string path = Path.Combine(sample.Directory, filePath);
                 File.Exists(path).Should().BeTrue($"File {path} should exist");
                 using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
                 SHA512.HashData(stream, output);
