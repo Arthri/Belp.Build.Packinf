83598c26bcf931d3604ce8a8272aa794cd03df62
Arthri <41360489+a@users.noreply.github.com>
2024-06-08 05:46:50 +0000

    Add tests for BLP4013

--- tests/Belp.Build.Packinf.UnitTests/PackageAssetTests.cs
+++ tests/Belp.Build.Packinf.UnitTests/PackageAssetTests.cs
@@ -38,4 +38,19 @@ public class PackageAssetTests
             Diagnostic.Error("BLP4012", "PackageAsset does not support _Package in the paths of package files.", Path.Combine(project.MSBuildProject.DirectoryPath, "Assets", "_Package", "x"), default, project.MSBuildProject.FullPath)
         ]);
     }
+
+    [Fact]
+    public void Expect_BLP4013_when_AssetsX2Fcontent_contains_an_extensionless_file()
+    {
+        TestProjectInstance project = MSBuildTest.Load.Project.From.Samples("BLP4013");
+        MSBuildResult result = project.Build();
+
+        using var scope = new AssertionScope();
+
+        result.OverallResult.Should().Be(BuildResultCode.Failure);
+
+        result.Diagnostics.Should().BeEquivalentTo([
+            Diagnostic.Error("BLP4013", "Content assets must have an extension.", Path.Combine(project.MSBuildProject.DirectoryPath, "Assets", "content", "x"), default, project.MSBuildProject.FullPath)
+        ]);
+    }
 }
